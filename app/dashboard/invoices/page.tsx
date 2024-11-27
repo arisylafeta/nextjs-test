ba94214319d3a3bbef2553c4ffa619467b860ac1
Arianit Sylafeta <ari.sylafeta@gmail.com>
2024-11-27 03:00:05 +0000

    fix some build bugsv0.1

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,22 +1,27 @@
-import { Suspense } from 'react';
-import { fetchInvoicesPages } from '@/app/lib/data';
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: {
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
+export default async function Page(props: {
+  searchParams?: Promise<{
     query?: string;
     page?: string;
-  };
+  }>;
 }) {
+  const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+
   const totalPages = await fetchInvoicesPages(query);
 
   return (
